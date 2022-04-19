403a246eccb78f6754cf494322ae48dc1b9548ff
Alan4245 <alanminecraft77@gmail.com>
2022-04-19 23:03:43 +0200

    Creata la classe contatto

---
+++ Progetto_Socket/Contatto.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Progetto_Socket
+{
+    class Contatto
+    {
+
+        private IPEndPoint _endPoint;
+        private string _nominativo;
+        private List<string> _chat;
+
+        public Contatto(IPEndPoint endPoint, string nominativo)
+        {
+
+            try
+            {
+                EndPoint = endPoint;
+                Nominativo = nominativo;
+            }catch(Exception ex)
+            {
+                throw new Exception("Impossibile creare il contatto: " + ex.Message);
+            }
+
+        }
+
+        public IPEndPoint EndPoint
+        {
+
+            get
+            {
+                return _endPoint;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    _endPoint = value;
+                }
+                else
+                {
+                    throw new Exception("Endpoint non valido!");
+                }
+            }
+
+        }
+
+        public string Nominativo
+        {
+            get
+            {
+                return _nominativo;
+            }
+
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value) == false)
+                {
+                    _nominativo = value;
+                }
+                else
+                {
+                    throw new Exception("Nominativo non valido!");
+                }
+            }
+        }
+
+        public List<string> Chat
+        {
+            get
+            {
+                return _chat;
+            }
+
+            set
+            {
+                if(value != null)
+                {
+                    _chat = value;
+                }
+                else
+                {
+                    throw new Exception("Chat nulla!");
+                }
+            }
+        }
+
+        public void AggiungiMessaggio(string messaggio)
+        {
+
+            _chat.Add(messaggio);
+
+        }
+
+    }
+}
